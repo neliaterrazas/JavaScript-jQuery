0757c90c4088cb445801f6dd9bb5f6106c429e03
Nelia Terrazas <terrazasnelia@gmail.com>
2019-01-18 16:22:20 -0800

    add html div and ids

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  $("#age").submit(function(event) {
+    var age =parseInt($("#yourAge").val());
+    if(age <=15) {
+      $('#groupOne').show
+      $(".")
+
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+  });
+
+});
