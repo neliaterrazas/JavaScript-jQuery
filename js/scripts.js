ac17c17a95c1a55c48b7a818e5963fbfcb341e55
Nelia Terrazas <terrazasnelia@gmail.com>
2019-01-24 12:23:32 -0800

    add backgroud image

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
+//The only variable that will determine the output is selecting a number from 1-9.
+
 $(document).ready(function() {
-  $("#age").submit(function(event) {
-    var age =parseInt($("#yourAge").val());
-    if(age <=15) {
-      $('#groupOne').show
-      $(".")
+  var number = parseInt($("#number").val());
+
+//User Interface Logic
+  $("form#survey").submit(function(event) {
+    if(number <=3) {
+      return ;
+    }if else(number<=6) {
+      return ;
+    }else {
+      return ;
 
     }
-  }
-
-
-
-
-
-
-
-
 
 
   });
-
+    $("results").text(number);
 });
